c22b419469190a023dcf1635fab03a159b2f1b3a
S_Shomo <90357187+SShomo@users.noreply.github.com>
2023-10-30 13:40:50 -0400

    Grid Work-ish

--- AI Girlies/Assets/Scripts/Grid/GridManager.cs
+++ AI Girlies/Assets/Scripts/Grid/GridManager.cs
@@ -7,19 +7,34 @@ public class GridManager : MonoBehaviour
     public int numRows = 10;
     public int numColumns = 10;
 
-    public float padding = 0.0001f;
+    public float padding = 2f;
 
     [SerializeField] private GridTile tilePrefab;
     public GridTile[] tiles;
 
+    private void Awake()
+    {
+        InitalizeGrid();
+    }
     public void InitalizeGrid()
     {
+        int p = 0;
+
         tiles = new GridTile[numRows * numColumns];
         for (int i = 0; i < numRows; i++)
         {
+            p = 0;
             for(int j = 0; j < numColumns; j++)
             {
+                GridTile tile = Instantiate(tilePrefab, transform);
+                Vector2 tilePos;
+                tilePos = new Vector2(i + (padding * i), j + (padding * j));
+          
 
+                tile.transform.localPosition = tilePos;
+                tile.name = $"Tile_{p + (j % 2)}_{j}";
+                tile.gridCords = new Vector2Int(p + (j % 2), j);
+                tiles[j * numColumns + i] = tile;
             }
         }
     }
