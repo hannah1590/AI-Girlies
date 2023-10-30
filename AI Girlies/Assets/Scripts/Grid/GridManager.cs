bf3e77e9444fa8a4433d19bef9ca7f8c9825458f
S_Shomo <90357187+SShomo@users.noreply.github.com>
2023-10-30 13:17:50 -0400

    grid stuff

---
+++ AI Girlies/Assets/Scripts/Grid/GridManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int numRows = 10;
+    public int numColumns = 10;
+
+    public float padding = 0.0001f;
+
+    [SerializeField] private GridTile tilePrefab;
+    public GridTile[] tiles;
+
+    public void InitalizeGrid()
+    {
+        //tiles;
+    }
+}
